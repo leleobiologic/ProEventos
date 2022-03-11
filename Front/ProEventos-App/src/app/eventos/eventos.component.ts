ed6e469ca70adc863d55839f0de97bf76975c332
Leonardo Alves <leo_h@DESKTOP-9U61GDO>
2022-03-11 00:38:11 -0300

    Angular att

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,13 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       (error) => console.log(error)
     );
+  }
+
+  alterarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
   }
   constructor(private http: HttpClient) {}
 
